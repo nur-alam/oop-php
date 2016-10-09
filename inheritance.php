84f9a90c29aa25a1ff17d5204e9823ce0ef62e16
nuralam <nuralam862@gmail.com>
2016-10-10 00:00:59 +0600

    learn abstruct

--- inheritance.php
+++ inheritance.php
@@ -7,23 +7,32 @@ class superClass{
 
 	public function __construct($name,$age)		
 	{
+
 		$this->name = $name;
 		$this->age = $age;
+
 	}
 
 	public function display()
 	{
+
 		echo "Name is {$this->name} and age is {$this->age} ";
+
 	}
 
 }
 
 class childClass extends superClass{
+
 	public $studied_at = 'software engineering';
+
 	public function display()
 	{
+
 		echo "Name is {$this->name} and age is {$this->age} and studied at {$this->studied_at}";
+
 	}
+
 }
 
 $obj1 = new superClass('nur','23');
@@ -36,6 +45,11 @@ $obj2 = new childClass('alam','22');
 
 $obj2->display();
 
+echo "</br>";
+
+if($obj2 instanceof superClass){
+	echo "yes inherited";
+}
 
 
 ?>
